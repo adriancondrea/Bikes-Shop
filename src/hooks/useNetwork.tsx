a652dc4f9b7b51adc78813a1fc0b3c76d792f517
Condrea Adrian <adriancondrea2000@gmail.com>
2021-12-06 23:52:16 +0200

    useNetwork.tsx change

--- src/hooks/useNetwork.tsx
+++ src/hooks/useNetwork.tsx
@@ -1,30 +1,40 @@
-import { useEffect, useState } from 'react';
-import { NetworkStatus, Plugins } from '@capacitor/core';
+import {useState} from 'react';
+import {Plugins} from '@capacitor/core';
 
-const { Network } = Plugins;
+const {Network} = Plugins;
 
 const initialState = {
-  connected: false,
-  connectionType: 'unknown',
+    connected: false,
+    connectionType: 'unknown',
 }
 
 export const useNetwork = () => {
-  const [networkStatus, setNetworkStatus] = useState(initialState)
-  useEffect(() => {
-    const handler = Network.addListener('networkStatusChange', handleNetworkStatusChange);
-    Network.getStatus().then(handleNetworkStatusChange);
-    let canceled = false;
-    return () => {
-      canceled = true;
-      handler.remove();
-    }
+  const [networkStatus, setNetworkStatus] = useState<boolean>(true);
+  Network.getStatus().then(status => setNetworkStatus(status.connected));
+  Network.addListener('networkStatusChange', (status) => {
+    setNetworkStatus(status.connected);
+  })
 
-    function handleNetworkStatusChange(status: NetworkStatus) {
-      console.log('useNetwork - status change', status);
-      if (!canceled) {
-        setNetworkStatus(status);
-      }
-    }
-  }, [])
-  return { networkStatus };
+  return {networkStatus};
 };
+
+// export const useNetwork = () => {
+//     const [networkStatus, setNetworkStatus] = useState(initialState)
+//     useEffect(() => {
+//         const handler = Network.addListener('networkStatusChange', handleNetworkStatusChange);
+//         Network.getStatus().then(handleNetworkStatusChange);
+//         let canceled = false;
+//         return () => {
+//             canceled = true;
+//             handler.remove();
+//         }
+//
+//         function handleNetworkStatusChange(status: NetworkStatus) {
+//             console.log('useNetwork - status change', status);
+//             if (!canceled) {
+//                 setNetworkStatus(status);
+//             }
+//         }
+//     }, [])
+//     return {networkStatus};
+// };
